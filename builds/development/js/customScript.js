497b6e0e83614674b5838b729439230328e11341
Ahmed Shaarawy <ahmedshaarawy891@gmail.com>
2017-10-24 21:06:57 +0200

    Fixing Responsive

--- builds/development/js/customScript.js
+++ builds/development/js/customScript.js
@@ -2,7 +2,15 @@ $(document).ready(function () {
     $('.menu_toggle').click(function () {
         $('body').toggleClass('menu_opened')
     });
+
+    // datepicker
     $(function () {
         $('.datepicker').datetimepicker();
+    });
+
+    // Message Contact List
+    $('.contacts_list_Toggle').click(function () {
+        $(this).toggleClass('active')
+        $('.messages_centre .contacts_list').toggleClass('active')
     });
 });
--- components/scripts/customScript.js
+++ components/scripts/customScript.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
+    // Menu
     $('.menu_toggle').click(function(){
         $('body').toggleClass('menu_opened')
+    });
+
+    // Message Contact List
+    $('.contacts_list_Toggle').click(function(){
+        $('.messages_centre .contacts_list').addClass('active')
     });
 });
